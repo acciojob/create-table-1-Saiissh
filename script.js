3da3afb6cf1e77caadad633a889260e42dcd140c
Saiissh <118446344+Saiissh@users.noreply.github.com>
2023-06-18 15:32:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function insert_Row() {
     //Write your code here
-  
-  
+  // function insertRow() {
+  var table = document.getElementById("sampleTable");
+  var newRow = table.insertRow(1); // Insert at index 1 to add it at the top
+
+  var cell1 = newRow.insertCell(0);
+  var cell2 = newRow.insertCell(1);
+
+  cell1.innerHTML = "New Row, Cell 1";
+  cell2.innerHTML = "Cell 2";
 }
+  
+// }
