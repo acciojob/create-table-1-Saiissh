723467b9b3ec7a3df605f395fddba9d5771adc53
Saiissh <118446344+Saiissh@users.noreply.github.com>
2023-06-18 15:37:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ function insert_Row() {
     //Write your code here
   // function insertRow() {
   var table = document.getElementById("sampleTable");
-  var newRow = table.insertRow(1); // Insert at index 1 to add it at the top
+  var newRow = table.insertRow(0); // Insert at index 1 to add it at the top
 
   var cell1 = newRow.insertCell(0);
   var cell2 = newRow.insertCell(1);
 
-  cell1.innerHTML = "New Row, Cell 1";
-  cell2.innerHTML = "Cell 2";
+  cell1.innerHTML = "New Cell1";
+  cell2.innerHTML = "New Cell2";
 }
   
 // }
